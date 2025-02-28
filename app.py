4d8adc073ebf3c833661fb9a60e80fe2e988ea68
sabonawebsite-com <sabonamarara53@gmail.com>
2025-02-28 21:28:24 +0300

    first commit

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy  
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user  
 from werkzeug.security import generate_password_hash, check_password_hash  
-
 app = Flask(__name__)  
 app.config['SECRET_KEY'] = 'your_secret_key'  
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'  
